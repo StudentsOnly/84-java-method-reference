f3dd5a2d424df958080a4d7e3cae9d5586db7259
bruch-alex <alex.bruch@tutanota.com>
2024-10-14 12:08:15 +0200

    solution

--- Main.java
+++ Main.java
@@ -1,1 +1,31 @@
+class Main {
+    public static void main(String[] args) {
 
+        // lambda
+        System.out.println(MathOperations.performOperation(5, 2, (a, b) -> MathOperations.addNumbers(a, b)));
+
+        // method reference
+        System.out.println(MathOperations.performOperation(5, 3, MathOperations::substractNumbers));
+        System.out.println(MathOperations.performOperation(5, 3, MathOperations::multiplyNumbers));
+
+    }
+}
+
+class MathOperations {
+
+    static int addNumbers(int a, int b) {
+        return a + b;
+    }
+
+    static int substractNumbers(int a, int b) {
+        return a - b;
+    }
+
+    static int multiplyNumbers(int a, int b) {
+        return a * b;
+    }
+
+    static int performOperation(int a, int b, MathOperation mathOperation) {
+        return mathOperation.execute(a, b);
+    }
+}
---
+++ MathOperation.java
@@ -0,0 +1,5 @@
+@FunctionalInterface
+public interface MathOperation {
+
+    int execute(int a, int b);
+}
